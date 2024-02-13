025dfe9d96819656bce23522dec9264b49a015e7
ashenafi <ashenafi.maru2020@gmail.com>
2024-02-13 18:52:27 -0500

    route added

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,25 @@ import './App.css'
 import FooterComponent from './components/FooterComponent'
 import HeaderComponent from './components/HeaderComponent'
 import ListPersonComponent from './components/ListPersonComponent'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 function App() {
   
   return (
     <>
-      <HeaderComponent/>
-      <ListPersonComponent/>
-      <FooterComponent/>
+      <BrowserRouter>
+        <HeaderComponent/>
+          <Routes>
+            {/* // http://localhost:3000 */}
+            <Route path='/' element = {<ListPersonComponent/>}></Route>
+
+            {/* // http://localhost:3000/persons */}
+            <Route path='/persons' element = {<ListPersonComponent/>}></Route>
+          </Routes>
+        
+        <FooterComponent/>        
+      </BrowserRouter>
+      
     </>
   )
 }
