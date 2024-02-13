dee4290e4f952b3b910b5e3616160741213dd35e
ashenafi <ashenafi.maru2020@gmail.com>
2024-02-13 18:32:58 -0500

    Header component added

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 
 import './App.css'
 import HelloWorld from './HelloWorld'
+import HeaderComponent from './components/HeaderComponent'
 import ListPersonComponent from './components/ListPersonComponent'
 
 function App() {
   
   return (
     <>
+      <HeaderComponent/>
       <ListPersonComponent/>
     </>
   )
